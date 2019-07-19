44af64475aece53971ece7a082e9830266d0aca8
Alexander Gusev <goooseman@me.com>
2019-07-20 00:04:08 +0300

    chore(tslint): linted

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -6,7 +6,7 @@ describe("identical", () => {
   it("should fail on additional properties (type)", () => {
     const T = excess(t.type({ foo: t.string }));
     assertFailure(T, { foo: "1", bar: "2" }, [
-      'Invalid value {"foo":"1","bar":"2"} supplied to : { foo: string }, excess properties: ["bar"]',
+      "Invalid value {\"foo\":\"1\",\"bar\":\"2\"} supplied to : { foo: string }, excess properties: [\"bar\"]",
     ]);
   });
 });
